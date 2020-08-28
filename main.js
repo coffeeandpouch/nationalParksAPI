4af1acdcb96bdeb2e9a8541cbb19ffc97cd5ba6f
SAM EINSIDLER <sameinsidler@SAMs-Air.attlocal.net>
2020-08-28 15:30:10 -0500

    latest changes

---
+++ main.js
@@ -0,0 +1,8 @@
+"use strict";
+
+const apiKey = "Goc3DcYDJubPmzgCIHAUAFibev24LxWvhsXAfaXk";
+
+const searchUrl =
+  "https://developer.nps.gov/api/v1/parks?parkCode=acad&api_key=Goc3DcYDJubPmzgCIHAUAFibev24LxWvhsXAfaXk";
+
+formatQueryParams(params);
