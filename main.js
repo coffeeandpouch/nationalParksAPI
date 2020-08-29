c30e90abe795db61370270d92128b86e71e3b505
SAM EINSIDLER <sameinsidler@SAMs-Air.attlocal.net>
2020-08-29 13:26:30 -0500

    latest changes

--- main.js
+++ main.js
@@ -2,7 +2,66 @@
 
 const apiKey = "Goc3DcYDJubPmzgCIHAUAFibev24LxWvhsXAfaXk";
 
-const searchUrl =
-  "https://developer.nps.gov/api/v1/parks?parkCode=acad&api_key=Goc3DcYDJubPmzgCIHAUAFibev24LxWvhsXAfaXk";
+const searchUrl = "https://developer.nps.gov/api/v1/parks";
+function formatQueryParams(params) {
+  const queryItems = Object.keys(params).map(
+    (key) => `${encodeURIComponent(key)}=${encodeURIComponent(params[key])}`
+  );
+  return queryItems.join("&");
+}
 
-formatQueryParams(params);
+function displayResults(responseJson) {
+  //if there are previous results, remove them
+  console.log(responseJson);
+  $("reults-list").empty();
+  //iterate through the items array
+  for (let i = 0; i < responseJson.data.length; i++) {
+    //for each park in the items array, add a list item to the results list
+    //with  the park code, state code, and full name
+    $("#results-list").append(
+      `<li><h3>${responseJson.data[i].fullName}</h3>
+        <p>${responseJson.data[i].description}</p>
+        <p>${responseJson.data[i].url}</p>
+        </li>`
+    );
+  }
+  //display the results section
+  $("#results").removeClass("hidden");
+}
+
+function getParks(query, maxResults) {
+  const params = {
+    api_key: apiKey,
+    parkCode: query,
+    // stateCode: query,
+    maxResults,
+  };
+  const queryString = formatQueryParams(params);
+  const url = searchUrl + "?" + queryString;
+
+  fetch(url)
+    .then((response) => {
+      if (response.ok) {
+        return response.json();
+      }
+      throw new Error(response.statusText);
+    })
+    .then((responseJson) => displayResults(responseJson))
+    .catch((err) => {
+      $("#js-error-message").text(`something went wrong: ${err.message}`);
+    });
+}
+
+function watchForm() {
+  $("form").submit((event) => {
+    event.preventDefault();
+    const searchTerm = $("#js-search-term").val();
+    const maxResults = $("#js-max-results").val();
+    getParks(searchTerm, maxResults);
+  });
+}
+
+$(function () {
+  console.log("App loaded! Waiting for submit!");
+  watchForm();
+});
