fcf892cb5b8c978b8749018dd1de2029cbf9dc7a
SAM EINSIDLER <sameinsidler@SAMs-MacBook-Air.local>
2020-09-07 13:24:58 -0500

    latest changes

--- main.js
+++ main.js
@@ -56,6 +56,13 @@ function getParks(query, limit) {
     });
 }
 
+// $(function){
+//     $("submit").click(function(event)){
+//         event.stopPropogation();
+        
+
+    }
+}
 function watchForm() {
   $("form").submit((event) => {
     event.preventDefault();
